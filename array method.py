35496749179405b60a5c0bc7a88f69560eb5b7d6
sarvesh pandey <95932509+sarveshpandey17@users.noreply.github.com>
2022-04-02 08:20:00 +0200

    Add files via upload

---
+++ array method.py
@@ -0,0 +1,35 @@
+#two types of method in array
+#import array
+#array_obj=array.array('i',[101,102,103,104])
+#print(array_obj)
+
+     #2nd method
+#from array import*
+#array_obj=array('i',[101,102,103])
+#print(array_obj)
+
+#indexing of the array
+
+#from array import*
+#array_obj=array('i',[101,102,103])
+#print(array_obj[0])
+#print(array_obj[1])
+#print(array_obj[2])
+
+#indexing array in loop
+#from array import*
+#array_obj=array('i',[101,102,103,104,105])
+#n=len(array_obj)
+#for i in range(n):
+#    print(array_obj[i])
+
+#while loop indexing
+#from array import*
+#array_obj=array('i',[101,102,103,104,105])
+#n=len(array_obj)
+#i=0
+#while(i<n):
+#    print(array_obj[i])
+#    i=i+1
+
+   
